933ba91c7dba59602d3617baee9d014d12db906f
Dmitri Guskov <dsguskov@bk.ru>
2023-12-31 03:21:20 +0300

    HW4: added solution to 10 and 11

--- home004/10_season1.c
+++ home004/10_season1.c
@@ -21,5 +21,31 @@
 
 int main(void)
 {
+	int input;
+	scanf("%d", &input);
+	switch (input)
+	{
+		case 12:
+		case 1:
+		case 2:
+			printf("winter\n");
+			break;
+		case 3:
+		case 4:
+		case 5:
+			printf("spring\n");
+			break;
+		case 6:
+		case 7:
+		case 8:
+			printf("summer\n");
+			break;
+		case 9:
+		case 10:
+		case 11:
+			printf("autumn\n");
+			break;
+		default:;
+	}
 	return 0;
 }
