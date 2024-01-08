0736fc2d53075108d8abe616e4579c88061d2737
Dmitri Guskov <dsguskov@bk.ru>
2024-01-08 18:13:43 +0300

    HW5: complete 12

--- home005/B12.c
+++ home005/B12.c
@@ -28,7 +28,24 @@ B12
 
 #include <stdio.h>
 
+#define BASE	10
+
 int main(void)
 {
+	int num;
+	scanf("%d", &num);
+
+	int min, max;
+	min = max = num % BASE;
+	num /= BASE;
+	for (int digit; num > 0; num /= BASE)
+	{
+		digit = num % BASE;
+		min = digit < min ? digit : min;
+		max = digit > max ? digit : max;
+	}
+
+	printf("%d %d\n", min, max);
+
 	return 0;
 }
