e53bcd001d6a63c0f7862cbd5244b7f3bcae9884
Dmitri Guskov <dsguskov@bk.ru>
2024-01-15 19:02:17 +0300

    HW6: added C10

--- home006/C10.c
+++ home006/C10.c
@@ -17,11 +17,30 @@ void print_simple(int n)
 */
 
 #include <stdio.h>
+#include <math.h>
+
+/// https://www.geeksforgeeks.org/print-all-prime-factors-of-a-given-number/
+
+void print_prime_factors(int n)
+{
+	for (; !(n % 2); n /= 2)
+		printf("2 ");
+
+	for (int i = 3; i >= sqrt(i); i += 2)
+		for (; !(n % i); n /= i)
+			printf("%d ", i);
+
+	if (n > 2)
+		printf("%d ", n);
+
+	puts("");
+}
+
 
 int main(void)
 {
 	int number;
 	scanf("%d", &number);
-
+	print_prime_factors(number);
 	return 0;
 }
