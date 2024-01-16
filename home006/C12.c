bf7139f9c7f9f1c0ea77e69854c6db0c2e20013d
Dmitri Guskov <dsguskov@bk.ru>
2024-01-16 10:22:25 +0300

    HW6: work-in-progress C12

--- home006/C12.c
+++ home006/C12.c
@@ -24,11 +24,34 @@ sin(x) = x - x**3/3! + x**5/5! - x**7/7! + …
 */
 
 #include <stdio.h>
+#include <math.h>
+
+double sin_taylor(int deg, double eps)
+{
+
+//	printf("%d %.2lf", deg, eps);
+	double rad = deg / 180. * M_PI;
+	printf(" %.2lf %.2lf\n", rad, eps);
+
+	double sum = 0;
+	double q = rad;
+
+	for (int i = 1;
+			fabs(q) > 0;
+			i++)
+	{
+		sum += q;
+		q *= (-1)*(rad*rad) / (2*i+1)*(2*i);
+	}
+	return sum;;
+}
+
 
 int main(void)
 {
 	int number;
 	scanf("%d", &number);
-
+	printf("%.10lf = sin_taylor\n", sin_taylor(number, 0.0001));
+	printf("%.10lf = sin of math.h\n", sin(number));
 	return 0;
 }
