2171d0dbddff546888496ef475d0a9c7b130ac70
Dmitri S. Guskov <dsguskov@bk.ru>
2024-01-17 14:41:43 +0300

    HW6: C12 updated (still WIP)

--- home006/C12_.c
+++ home006/C12_.c
@@ -1,35 +1,37 @@
 /*
-C12 Демо
-Вычислить sin
-Составить функцию, которая вычисляет синус как сумму ряда (с точностью 0.001)
+   C12 Демо
+   Вычислить sin
+   Составить функцию, которая вычисляет синус как сумму ряда (с точностью 0.001)
 
-sin(x) = x - x**3/3! + x**5/5! - x**7/7! + …
-(x в радианах)
-Прототип функции: float sinus(float x)
+   sin(x) = x - x**3/3! + x**5/5! - x**7/7! + …
+   (x в радианах)
+   Прототип функции: float sinus(float x)
 
-Данные на входе: 		Одно число от 0 до 90 
-Данные на выходе: 	Вещественное число в формате "%.3f" 
+   Данные на входе: 		Одно число от 0 до 90 
+   Данные на выходе: 	Вещественное число в формате "%.3f" 
 
-Пример №1
-Данные на входе: 		45 
-Данные на выходе: 	0.707 
+   Пример №1
+   Данные на входе: 		45 
+   Данные на выходе: 	0.707 
 
-Пример №2
-Данные на входе: 		30 
-Данные на выходе: 	0.500 
+   Пример №2
+   Данные на входе: 		30 
+   Данные на выходе: 	0.500 
 
-Пример №3
-Данные на входе: 		0 
-Данные на выходе: 	0.000
-*/
+   Пример №3
+   Данные на входе: 		0 
+   Данные на выходе: 	0.000
+   */
 
 #include <stdio.h>
+
+#define _USE_MATH_DEFINES
 #include <math.h>
 
 double sin_taylor(int deg, double eps)
 {
 
-//	printf("%d %.2lf", deg, eps);
+	//	printf("%d %.2lf", deg, eps);
 	double rad = deg / 180. * M_PI;
 	printf(" %.2lf %.2lf\n", rad, eps);
 
@@ -43,7 +45,7 @@ double sin_taylor(int deg, double eps)
 		sum += q;
 		q *= (-1)*(rad*rad) / (2*i+1)*(2*i);
 	}
-	return sum;;
+	return sum;
 }
 
 
@@ -51,7 +53,7 @@ int main(void)
 {
 	int number;
 	scanf("%d", &number);
-	printf("%.10lf = sin_taylor\n", sin_taylor(number, 0.0001));
-	printf("%.10lf = sin of math.h\n", sin(number));
+	printf("%.3lf = sin_taylor\n", sin_taylor(number, 0.0001));
+	printf("%.3lf = sin of math.h\n", sin(number));
 	return 0;
 }
