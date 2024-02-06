85ee6b6903068e78f6c21f01ee0b620ecf5a1151
Dmitri Guskov <dsguskov@bk.ru>
2024-02-06 16:32:09 +0300

    HW9: space preparation

---
+++ home009/compile1.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [[ $# -eq 0 ]]
+then
+    echo "No argument specified"
+    exit 1
+fi
+
+mkdir build 2>/dev/null
+
+OPTS='-std=c99 -Wall -Werror -Wextra -Wpedantic'
+
+set -x
+while true
+do
+  gcc    $OPTS -o build/$1 $1.c || break
+  break
+done
+set +x
