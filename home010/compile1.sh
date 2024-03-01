3c11f5f439cb5c4e843c0f6392068cf2349b8f9a
Dmitri Guskov <dsguskov@bk.ru>
2024-03-01 20:34:40 +0300

    compile1: added lines for generating .s and .i
    .s is an assembly file
    .i is an intermediate (preprocessed) file

--- home010/compile1.sh
+++ home010/compile1.sh
@@ -13,6 +13,8 @@ OPTS='-std=c99 -Wall -Werror -Wextra -Wpedantic -Wconversion'
 set -x
 while true
 do
+  gcc -S -mllvm --x86-asm-syntax=intel $1.c || break
+  cpp $1.c > $1.i || break
   gcc    $OPTS -o $1 $1.c || break
   break
 done
