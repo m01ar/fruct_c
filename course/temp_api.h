f53834f9e6009c001f8a97568523f065e6336471
Dmitri S. Guskov <dsguskov@bk.ru>
2024-03-10 22:02:14 +0300

    temp_api.h: modified month data type from enumeration to unsigned char

--- course/temp_api.h
+++ course/temp_api.h
@@ -3,28 +3,20 @@
 
 #include <stddef.h>
 
-enum months
-{
-    UNDEFINED,
-    JAN, FEB, MAR, APR, MAY, JUN,
-    JUL, AUG, SEP, OCT, NOV, DEC
-};
+// enum months
+// {
+//     UNDEFINED,
+//     JAN, FEB, MAR, APR, MAY, JUN,
+//     JUL, AUG, SEP, OCT, NOV, DEC
+// };
 
 typedef unsigned short int year_t;
-typedef enum months        month_t;
+//typedef enum months        month_t;
+typedef unsigned char      month_t;
 typedef unsigned char      day_t;
 typedef unsigned char      hour_t;
 typedef unsigned char      minute_t;
-typedef char               temp_t;
-
-/* Структура типа
- * dddd        - год 4 цифры,
- * mm          - месяц 2 символа,
- * dd          - день 2 цифры,
- * hh          - часы 2 цифры
- * mm          - минуты 2 цифры
- * temperature - целое число от -99 до 99
-*/
+typedef signed char        temp_t;
 
 typedef struct
 {
@@ -36,17 +28,6 @@ typedef struct
     temp_t   temperature;
 }
 record_s;
-
-/*  Прототипы (заглушки) функций
- * - вывода статистики по каждому месяцу:
- *   - среднемесячная температура;
- *   - минимальная температура в текущем месяце;
- *   - максимальная температура в текущем месяце;
- * - вывода статистики за год:
- *   - среднегодовая температура;
- *   - минимальная температура;
- *   - максимальная температура;
-*/
 
 temp_t month_avg_temp(record_s *, size_t, month_t);
 temp_t month_min_temp(record_s *, size_t, month_t);
